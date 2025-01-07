426d5e5a0b033169a3802c82ffb4f949285c245b
Navin Kumar Erelli <radohyd1610@gmail.com>
2025-01-07 15:31:17 +0100

    Update samplecode.py

--- samplecode.py
+++ samplecode.py
@@ -1,11 +1,9 @@
-def test_addition(a, b):
+# app.py
+# This is a test commit
+# I am testing commit
+def add(a, b):
     return a + b
 
-# This test will pass
-assert test_addition(1, 3) == 4  # Passes, since 1 + 3 == 4
-
-# This test will fail (intentionally)
-try:
-    assert test_addition(1, 3) == 5  # Raises AssertionError
-except AssertionError:
-    print("Test failed: test_addition(1, 3) != 5")
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
